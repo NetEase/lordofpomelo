c7373b171b272357bdd2a2aedd77d215bc0ac77a
xiecc <xieccy@gmail.com>
2013-01-18 13:45:28 +0800

    modify config for image
    Change-Id: If33892d1b1fea0743998c2d3270b6742ab2fe60c

--- web-server/public/js/config/config.js
+++ web-server/public/js/config/config.js
@@ -1,6 +1,6 @@
 __resources__["/config.js"] = {meta: {mimetype: "application/javascript"}, data: function(exports, require, module, __filename, __dirname) {
 	module.exports = {
-		//IMAGE_URL:'http://pomelo.netease.com/art/',
+		IMAGE_URL:'http://pomelo.netease.com/art/',
 		//IMAGE_URL:'http://192.168.144.127:81/',
 		IMAGE_URL:'http://192.168.147.207:80/',
 		GATE_HOST: window.location.hostname,
