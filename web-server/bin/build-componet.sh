bd0b1a873f02077ad6edd34bb818ec8668b05ca3
fantasyni <fantasyni@163.com>
2013-02-26 16:50:29 +0800

    add component support

---
+++ web-server/bin/build-componet.sh
@@ -0,0 +1,1 @@
+cd public/js/lib &&  component build -v
---
+++ web-server/public/js/lib/local/boot/index.js
@@ -0,0 +1,8 @@
+  var Emitter = require('emitter');
+  window.EventEmitter = Emitter;
+
+  var pomelo = require('pomelo-jsclient-socket.io');
+  window.pomelo = pomelo;
+
+  var jquery = require('jquery');
+  window.$ = jquery;
