0235239d8c379908b01a29db93a552744196f8c0
palmtoy <zgli@corp.netease.com>
2013-08-26 15:56:37 +0800

    add webkit-devtools-agent for cpu profiling

--- game-server/app.js
+++ game-server/app.js
@@ -27,19 +27,6 @@ app.configure('production|development', function() {
 	app.enable('systemMonitor');
   require('./app/util/httpServer');
 
-  /*
-   kill -SIGUSR2 <pid>
-   http://localhost:8080/inspector.html?host=localhost:9999&page=0
-  */
-  /*
-  require('webkit-devtools-agent');
-  var express = require('express');
-  var expressSvr = express();
-  expressSvr.use(express.static(__dirname + '/26.0.1410.65'));
-  expressSvr.listen(8080);
-  console.log('[%s] Server running at http://127.0.0.1:8080/', process.pid);
-  */
-
 	//var sceneInfo = require('./app/modules/sceneInfo');
 	var onlineUser = require('./app/modules/onlineUser');
 	if(typeof app.registerAdmin === 'function'){
@@ -96,6 +83,16 @@ app.configure('production|development', 'area', function(){
 	}else{
 		scene.init(dataApi.area.findById(server.area));
 		app.areaManager = scene;
+    /*
+     kill -SIGUSR2 <pid>
+     http://localhost:8080/inspector.html?host=localhost:9999&page=0
+    */
+    require('webkit-devtools-agent');
+    var express = require('express');
+    var expressSvr = express.createServer();
+    expressSvr.use(express.static(__dirname + '/26.0.1410.65'));
+    var tmpPort = 3270 + parseInt(server.area);
+    expressSvr.listen(tmpPort);
 	}
 
 	//Init areaService
