af190459b7f48b2ddf14eeebd55d4abf5fa302e2
palmtoy <zgli@corp.netease.com>
2013-11-07 10:11:19 +0800

    * modify game-server's master server's ip

--- web-server/public/js/config/config.js
+++ web-server/public/js/config/config.js
@@ -1,7 +1,7 @@
 __resources__["/config.js"] = {meta: {mimetype: "application/javascript"}, data: function(exports, require, module, __filename, __dirname) {
 	module.exports = {
 		IMAGE_URL: 'http://pomelo.netease.com/art/',
-		GATE_HOST: 'pomelo17.server.163.org',
+		GATE_HOST: 'pomelo16.server.163.org',
 		GATE_PORT: 3014
 	};
 }};
