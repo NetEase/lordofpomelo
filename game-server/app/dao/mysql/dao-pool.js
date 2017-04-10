4a908d98eb4ab40be2026afd0833b3bfc29bb309
Wang Xinyong <wang.xy.chn@gmail.com>
2017-04-10 10:34:07 +0800

    get mysql config from env first

--- game-server/app/dao/mysql/dao-pool.js
+++ game-server/app/dao/mysql/dao-pool.js
@@ -5,16 +5,22 @@ var _poolModule = require('generic-pool');
  */
 var createMysqlPool = function(app) {
 	var mysqlConfig = app.get('mysql');
+	var host = process.env.LORD_MYSQL_HOST || mysqlConfig.host;
+	var port = process.env.LORD_MYSQL_PORT || mysqlConfig.port;
+	var user = process.env.LORD_MYSQL_USER || mysqlConfig.user;
+	var password = process.env.LORD_MYSQL_PASSWORD || mysqlConfig.password;
+	var database = process.env.LORD_MYSQL_DATABASE || mysqlConfig.database;
+
 	return _poolModule.Pool({
 		name: 'mysql',
 		create: function(callback) {
 			var mysql = require('mysql');
 			var client = mysql.createConnection({
-				host: mysqlConfig.host,
-				user: mysqlConfig.user,
-				password: mysqlConfig.password,
-				database: mysqlConfig.database,
-                                port: mysqlConfig.port
+				host: host,
+				user: user,
+				password: password,
+				database: database,
+				port: port
 			});
 			callback(null, client);
 		},
